31bca8dd977506e789205a74d57d44fcb8061245
Johan Larsson <johan_gavle1@hotmail.com>
2015-07-07 22:29:47 +0200

    Cleaned up templates a bit.

---
+++ Gu.PropertyTree.Demo/ValueTemplateSelector.cs
@@ -0,0 +1,33 @@
+﻿namespace Gu.PropertyTree.Demo
+{
+    using System.Windows;
+    using System.Windows.Controls;
+
+    public class ValueTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate NullTemplate { get; set; }
+      
+        public DataTemplate StringTemplate { get; set; }
+        
+        public DataTemplate ReadonlyStringTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            var node = item as IPropertyNode;
+            if (node == null)
+            {
+                return NullTemplate;
+            }
+
+            if (node.ParentProperty.CanWrite)
+            {
+                return StringTemplate;
+            }
+            else
+            {
+                return ReadonlyStringTemplate;
+            }
+            return base.SelectTemplate(item, container);
+        }
+    }
+}
